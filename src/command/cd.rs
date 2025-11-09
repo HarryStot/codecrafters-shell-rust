5fe12c2a24bc3fa1fc2669be76718b954412b5ce
Nathan <bourguignon.nathan@gmail.com>
2025-11-10 00:15:41 +0100

    codecrafters submit [skip ci]

--- src/command/cd.rs
+++ src/command/cd.rs
@@ -1,14 +1,30 @@
-use std::path::Path;
+use std::path::PathBuf;
+use std::env::var_os;
 use std::env::set_current_dir;
 use super::{Command, CommandError};
 
 pub(crate) fn cd_cmd(args: &str) {
-    // Absolute path
-    let path = Path::new(args);
+    let args = args.trim();
+    let target = if args.is_empty() {
+        // no argument -> HOME if set
+        var_os("HOME").map(|h| PathBuf::from(h))
+    } else if args.starts_with('~') {
+        // expand ~ to HOME if possible, otherwise treat literally without the ~
+        var_os("HOME").map(|h| {
+            let rest = args.strip_prefix('~').unwrap_or("");
+            PathBuf::from(h).join(rest)
+        }).or_else(|| Some(PathBuf::from(args.strip_prefix('~').unwrap_or(args))))
+    } else {
+        Some(PathBuf::from(args))
+    };
 
-    match set_current_dir(path) {
-        Ok(_t) => (),
-        Err(_e) => eprintln!("cd: {}: No such file or directory", path.display()),
+    match target {
+        Some(path) => {
+            if let Err(err) = set_current_dir(&path) {
+                eprintln!("cd: {}: {}", path.display(), err);
+            }
+        }
+        None => eprintln!("cd: {}: HOME not set", args),
     }
 }
 
