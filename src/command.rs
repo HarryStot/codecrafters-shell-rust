889a0dfd6e949704e32eefeac2dc8df1c70fac80
Nathan <bourguignon.nathan@gmail.com>
2025-11-10 00:54:06 +0100

    codecrafters submit [skip ci]

--- src/command.rs
+++ src/command.rs
@@ -40,19 +40,52 @@ impl Command {
         use Command::*;
         let input = input.trim().splitn(2, ' ').collect::<Vec<&str>>();
         let cmd = input.get(0).copied().unwrap_or("");
-        let args = input.get(1).copied().unwrap_or("");
+        let args_raw = input.get(1).copied().unwrap_or("");
+        let args = preprocess_args(args_raw);
+
         Ok(match cmd {
             "" => Noop,
-            "echo" => echo::parse_echo_cmd(args)?,
-            "exit" => exit::parse_exit_cmd(args)?,
-            "type" => typee::parse_type_cmd(args)?,
-            "pwd" => pwd::parse_pwd_cmd(args)?,
-            "cd" => cd::parse_cd_cmd(args)?,
-            _ => match external::parse_external_cmd(cmd, args) {
+            "echo" => echo::parse_echo_cmd(&args)?,
+            "exit" => exit::parse_exit_cmd(&args)?,
+            "type" => typee::parse_type_cmd(&args)?,
+            "pwd" => pwd::parse_pwd_cmd(&args)?,
+            "cd" => cd::parse_cd_cmd(&args)?,
+            _ => match external::parse_external_cmd(cmd, &args) {
                 Some(cmd) => cmd,
                 None => return Err(CommandError::NotFound(cmd.to_string())),
             },
         })
     }
 
+}
+
+fn preprocess_args(args: &str) -> String {
+    let mut out = String::with_capacity(args.len());
+    let mut in_quote = false;
+    let mut last_was_space = false;
+
+    for c in args.chars() {
+        match c {
+            '\'' => in_quote = !in_quote,
+            ' ' => {
+                if in_quote {
+                    out.push(' ');
+                    last_was_space = false;
+                } else if !last_was_space {
+                    out.push(' ');
+                    last_was_space = true;
+                }
+            }
+            _ => {
+                out.push(c);
+                last_was_space = false;
+            }
+        }
+    }
+
+    if last_was_space {
+        out.pop();
+    }
+
+    out
 }
